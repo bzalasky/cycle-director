39f0c876888c69f23b1450e53c8fdcc3e684e5cb
Tylor Steinberger <tylor@tylor-linux.home>
2015-08-15 13:15:11 -0400

    Update what is returned by router

--- src/index.js
+++ src/index.js
@@ -39,24 +39,18 @@ function createRouter$(router) {
     return Rx.Observable.merge(
       Rx.Observable.fromEvent(document.body, 'click')
     )
-    .startWith(getStartingURL(router))
-    .map((event)=>{
-      return "/" + router.getRoute();
-    })
   }
 
   return Rx.Observable.merge(
     Rx.Observable.fromEvent(document, 'load'),
     Rx.Observable.fromEvent(window, 'hashchange')
-  )
-  .startWith(getStartingURL(router))
-  .map((event) => {
-    console.log(event);
-    return "/" + router.getRoute();
-  });
+  );
+
+
+
 }
 
-function getStartingURL(router) {
+function getCurrentUrl(router) {
   let url;
   if (router.history) {
     url = window.location.pathname || "/";
@@ -66,38 +60,80 @@ function getStartingURL(router) {
   return url;
 }
 
-function addRoute(routes, route) {
+function wrapHandler(handler, subject) {
+
+  return (...params) => {
+    if (typeof handler === "function") {
+      subject.onNext(handler(...params));
+    } else if (Array.isArray(handler)){
+      handler.forEach((h) => {
+        subject.onNext(h(...params))
+      })
+    }
+  }
+
+  
+}
+
+
+function addRoute(routes, route, subject) {
 
   routes[route.url] = {
-    once: route.once || ()=>{},
-    before: route.before || ()=>{},
-    on: route.on || ()=>{},
-    after: route.after || ()=>{}
+    once: wrapHandler(route.once, subject),
+    before: wrapHandler(route.before, subject),
+    on: wrapHandler(route.on, subject),
+    after: wrapHandler(route.after, subject)
   }
 
 }
 
+function wrapOptionsMethods(routerOptions, subject) {
+  if (routerOptions.notfound) {
+      routerOptions.notfound = wrapHandler(routerOptions.notfound, subject);
+    } 
+
+    if (routerOptions.on) {
+      routerOptions.on = wrapHandler(routerOptions.on, subject);
+    }
+
+    if (routerOptions.before) {
+      routerOptions.before = wrapHandler(routerOptions.before, subject);
+    }
+
+    if (routerOptions.after) {
+      routerOptions.after = wrapHandler(routerOptions.after, subject);
+    }
+}
+
 function makeRouterDriver(routerOptions) {
+
   let router = new Router()
-    .configure(routerOptions);
 
   return function(route$) {
+    let subject = new Rx.Subject();
+
+    wrapOptionsMethods(routerOptions, subject)
 
     let routes = {};
+
     route$.subscribe(
     (route) => {
-      addRoute(routes, route);
+      addRoute(routes, route, subject);
     },
     (err) => {
       console.log(err);
     },
     () => {
       router.mount(routes);
-      router.init(getStartingURL(router));
+      router.configure(routerOptions);
+      if (routerOptions.before) {
+        routerOptions.before();
+      }
+      router.init(getCurrentUrl(router));
     });
 
-    let router$ = createRouter$(router);
-    return router$
+    
+    return subject;
   }
 }
 
